bb19857586a392ad39de28695ae8fb6bc1ef6efc
Victor Alcantara <vardeveloper@gmail.com>
2021-10-27 19:55:38 -0500

    improvements: environment variables

--- db.py
+++ db.py
@@ -1,15 +1,20 @@
 import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine('postgresql://{}:{}@{}:{}/{}'.format(
-    os.getenv('DB_USER', 'postgres'),
-    os.getenv('DB_PASS', 'postgres'),
-    os.getenv('DB_HOST', 'localhost'),
-    os.getenv('DB_PORT', 5432),
-    os.getenv('DB_NAME', 'jina_search')
-))
+load_dotenv()
+
+engine = create_engine(
+    "postgresql://{}:{}@{}:{}/{}".format(
+        os.getenv("DB_USER", "postgres"),
+        os.getenv("DB_PASS", "postgres"),
+        os.getenv("DB_HOST", "localhost"),
+        os.getenv("DB_PORT", 5432),
+        os.getenv("DB_NAME", "jina_search"),
+    )
+)
 
 Session = sessionmaker(bind=engine)
 session = Session()
