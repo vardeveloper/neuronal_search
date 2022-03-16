0b7028746099d3d03f1fd7476803e726372c76d4
Victor Alcantara <vitucho_6@hotmail.com>
2022-03-16 11:46:39 -0500

    fix: API PHP

--- index.php
+++ index.php
@@ -7,23 +7,27 @@
 ########################
 
 // HEADERS
-//header("Access-Control-Allow-Origin: *"); // CORS
+header("Access-Control-Allow-Origin: *"); // CORS
 header("Access-Control-Allow-Headers: *"); // CORS
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS"); // CORS
+header("Access-Control-Allow-Methods: GET, POST, PUT, OPTIONS"); // CORS
 header('Content-Type: application/json; charset=utf-8');
 
-$TOKEN = "Bearer bbf9537bc4b6b0a40c6665967cb9f759620cd611";
+// validate API KEY
+$API_KEY = "bbf9537bc4b6b0a40c6665967cb9f759620cd611";
 $headers = getallheaders();
-if ($headers["Authorization"] != $TOKEN) {
-    echo json_encode(array('status' => FALSE, 'message' => 'El TOKEN es incorrecto'));
+if (base64_decode($headers["API_KEY"]) != $API_KEY) {
+    echo json_encode(array('status' => FALSE, 'message' => 'El API_KEY es incorrecto'));
     exit();
 }
 
+// remove path 
 $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $path = str_replace("/apisearch/index.php/", "", $path);
 $path = str_replace("/", "", $path);
 
-if (strcasecmp($_SERVER['REQUEST_METHOD'], 'POST') == 0) {
+// get data of body
+if (strcasecmp($_SERVER['REQUEST_METHOD'], 'POST') == 0 ||
+    strcasecmp($_SERVER['REQUEST_METHOD'], 'PUT') == 0) {
     $content = trim(file_get_contents('php://input'));
     $data_array = array();
     if (!empty($content)) {
@@ -37,15 +41,36 @@ if (strcasecmp($_SERVER['REQUEST_METHOD'], 'POST') == 0) {
 ################
 
 $url = "http://10.128.0.17:8000/$path";
-$ch = curl_init($url);
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+
+if (strcasecmp($_SERVER['REQUEST_METHOD'], 'GET') == 0) {
+    $headers = getallheaders();
+    if ($headers["Authorization"]) {
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Authorization: ' . $headers["Authorization"], 'Content-Type: application/json'));
+        curl_setopt($ch, CURLINFO_HEADER_OUT, true);
+    }
+}
 
 if (strcasecmp($_SERVER['REQUEST_METHOD'], 'POST') == 0) {
+    if ($path == 'login/access-token') {
+        curl_setopt($ch, CURLOPT_POST, TRUE);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, array('username' => $_POST['username'], 'password' => $_POST['password']));
+    } else {
+        $payload = json_encode($data_array);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+        curl_setopt($ch, CURLOPT_POST, TRUE);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
+    }
+}
+
+if (strcasecmp($_SERVER['REQUEST_METHOD'], 'PUT') == 0) {
     $payload = json_encode($data_array);
-    curl_setopt($ch, CURLOPT_POST, TRUE);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json', 'Content-Length: ' . strlen($payload)));
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT');
     curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
 }
 
-curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
 
@@ -56,6 +81,12 @@ $response = curl_exec($ch);
 if ($response === FALSE) {
     die(curl_error($ch));
 }
+
+// Show what was sent in the header
+// if (!curl_errno($ch)) {
+//     $info = curl_getinfo($ch);
+//     print_r($info['request_header']);
+// }
 
 // close curl
 curl_close($ch);
@@ -72,7 +103,7 @@ if (isset($data_array["parameters"]["origin"]) && $data_array["parameters"]["ori
         if (empty($docs['matches'])) {
             echo json_encode(array('status' => FALSE, 'message' => 'No hay coincidencias'));
         } else {
-            //$docs['matches'][0]['tags']['answer'] = $docs['matches'][0]['tags']['answer'] . "\n\n¿Deseas realizar otra consulta?";
+            // $docs['matches'][0]['tags']['answer'] = $docs['matches'][0]['tags']['answer'] . "\n\n¿Deseas realizar otra consulta?";
             echo json_encode($docs['matches'][0]['tags']);
         }
     } else {
